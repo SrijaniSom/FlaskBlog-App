f33c6b58ebf782e5aa1c5c0332721c19226230ef
SrijaniSom <srijanisom2002@gmail.com>
2024-06-14 05:21:07 -0400

    feat/blueprint structure added

--- Flask Tutorials/flaskblog/users/forms.py
+++ Flask Tutorials/flaskblog/users/forms.py
@@ -1,10 +1,9 @@
-from flask import Flask
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email,EqualTo, ValidationError
-from flaskblog.models import User, Post
-from flask_wtf.file import FileAllowed, FileField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
@@ -47,11 +46,6 @@ class AccountUpdateForm(FlaskForm):
             user=User.query.filter_by(email=email.data).first()
             if(user):
                 raise ValidationError('That Email is Already taken!')
-            
-class PostForm(FlaskForm):
-    title=StringField('Title',validators=[DataRequired(),Length(min=4,max=30)])
-    content=TextAreaField('Content',validators=[DataRequired()])
-    submit=SubmitField('Upload')
 
 class RequestResetPasswordForm(FlaskForm):
     email = StringField('Email',
